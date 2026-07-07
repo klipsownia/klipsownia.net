10eaa7fe92191df831ecdf51b60d7d78c9201f2c
klipsownia <klipsownia@gmail.com>
2026-07-07 04:39:19 +0200

    Create run-once.js

---
+++ scraper/run-once.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { runAllTargets } = require('./engine');
+const { pool }          = require('../db/pool');
+const logger            = require('../config/logger');
+
+(async () => {
+  try {
+    await runAllTargets();
+    await pool.end();
+    process.exit(0);
+  } catch (e) {
+    logger.error(e.message);
+    await pool.end();
+    process.exit(1);
+  }
+})();
